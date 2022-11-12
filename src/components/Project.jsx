56332fc1293086be380d14a8e8d07ebdf8be6ed9
Stephen Okyere <thuqpalmer@gmail.com>
2022-11-12 08:32:37 +0000

    skills aligned

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -2,7 +2,7 @@ import React,{useEffect} from 'react'
 import AOS from "aos";
 import "aos/dist/aos.css";
 
-const Project = ({src,description,name,link,key}) => {
+const Project = ({src,description,name,link}) => {
    useEffect(() => {
     AOS.init();
     AOS.refresh();
@@ -13,7 +13,7 @@ const Project = ({src,description,name,link,key}) => {
     // border: `1px solid rgba(255, 255, 255, 0.125)`,
   }
   return (
-    <section className='backdrop-blur-[25px] backdrop-saturate-[200%] h-[320px]  md:h-[450px] bg-[rgba(17, 25, 40, 0.37)]  rounded-2xl overflow-hidden shadow-2xl snap-y scroll-mt-[7.5em] ' key={key} data-aos="zoom-in">
+    <section className='backdrop-blur-[25px] backdrop-saturate-[200%] h-[320px]  md:h-[450px] bg-[rgba(17, 25, 40, 0.37)]  rounded-2xl overflow-hidden shadow-2xl snap-y scroll-mt-[7.5em] ' data-aos="zoom-in">
         <img src={src} alt="" className='h-[200px] md:h-[320px] object-cover bg-cover w-full' />
         <article className='px-6 py-4 leading-[2em] '>
             <h3 className='font-semibold text-xl'>{name}</h3>
