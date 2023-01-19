a461c8ceb1109e5800ecdbd43b2f96dd36fb2d67
Stephen <105071093+stephenokyere233@users.noreply.github.com>
2023-01-19 01:19:17 +0000

    [fix] update links

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,15 +5,15 @@ import resume from "./resume.pdf"
 const Header = () => {
   return (
     <header className={`max-w-7xl mx-auto flex justify-between z-10 bg-white dark:bg-[#222] px-6 h-[5em]  items-center sticky top-0`}>
-        <a href="steve#">
+        <a href="#">
           <img src={logo} alt="" className="w-[50px]" />
         </a>
         <nav className=' hidden lg:flex gap-x-10 text-[18px] font-semibold'>
-            <a href="/steve#">Home</a>
-            <a href="/steve#about">About</a>
-            <a href="/steve#projects">Projects</a>
-            <a href="/steve#skills">Skills</a>
-            <a href="/steve#contact">Contact</a>
+            <a href="#">Home</a>
+            <a href="#about">About</a>
+            <a href="#projects">Projects</a>
+            <a href="#skills">Skills</a>
+            <a href="#contact">Contact</a>
         </nav>
         <a className='bg-purple-600 p-4 rounded-[100px]'href={resume} >
             Download CV
